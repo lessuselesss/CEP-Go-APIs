8a03f6363a5393bf481942571e6bb2fc1f3c09e4
lessuseless <179788364+lessuselesss@users.noreply.github.com>
2025-07-02 19:07:15 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,2 @@
 package circular_enterprise_protocol_api
 
-const (
-    libVersion    = "1.0.13"
-    networkURL    = "https://circularlabs.io/network/getNAG?network="
-    defaultChain  = "0x8a20baa40c45dc5055aeb26197c203e576ef389d9acb171bd62da11dc5ad72b2"
-    defaultNAG    = "https://nag.circularlabs.io/NAG.php?cep="
-)
